94d547cbce99b9528898de8f47ceb6bc9252e4e7
qoiqvi <111002830+qoiqvi@users.noreply.github.com>
2023-06-13 21:45:01 +0300

    add tests for LoginByUsername, add TestAsyncThunk

--- src/features/AuthByUserName/model/services/LoginByUsername/LoginByUsername.test.ts
+++ src/features/AuthByUserName/model/services/LoginByUsername/LoginByUsername.test.ts
@@ -1,24 +1,32 @@
 import axios from "axios"
 import { loginByUsername } from "./LoginByUsername"
-import type { Dispatch } from "@reduxjs/toolkit"
-import type { StateSchema } from "App/provider/StoreProvider"
+import { TestsAsyncThunk } from "shared/lib/tests/TestsAsyncThunk/TestsAsyncThunk"
+import { UserSliceActions } from "entities/User"
 
 jest.mock("axios")
-
 const mockedAxios = jest.mocked(axios, true)
+
 describe("LoginByUsername.test", () => {
-	let dispatch: Dispatch
-	let getState: () => StateSchema
-	beforeEach(() => {
-		dispatch = jest.fn()
-		getState = jest.fn()
-	})
 	test("should ", async () => {
-		mockedAxios.post.mockReturnValue(Promise.resolve({ data: { username: "123", id: "1" } }))
-		const action = loginByUsername({ username: "123", password: "123" })
-		const result = await action(dispatch, getState, undefined)
-		console.log(result)
-		// expect().toEqual()
+		const userValue = { username: "123", id: "1" }
+		mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }))
+		const thunk = new TestsAsyncThunk(loginByUsername)
+		const result = await thunk.callThunk({ username: "123", password: "123" })
+		expect(thunk.dispatch).toHaveBeenCalledWith(UserSliceActions.setAuthData(userValue))
+		expect(thunk.dispatch).toHaveBeenCalledTimes(3)
+		expect(mockedAxios.post).toHaveBeenCalled()
+		expect(result.meta.requestStatus).toBe("fulfilled")
+		expect(result.payload).toEqual(userValue)
+	})
+	test("error login", async () => {
+		mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }))
+		const thunk = new TestsAsyncThunk(loginByUsername)
+		const result = await thunk.callThunk({ username: "123", password: "123" })
+
+		expect(thunk.dispatch).toHaveBeenCalledTimes(2)
+		expect(mockedAxios.post).toHaveBeenCalled()
+		expect(result.meta.requestStatus).toBe("rejected")
+		expect(result.payload).toBe("error")
 	})
 })
 
