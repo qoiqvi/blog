2f69da02bd062415e533f6384f3254f5656d0711
qoiqvi <111002830+qoiqvi@users.noreply.github.com>
2023-06-13 21:54:47 +0300

    tests for loginSlice

---
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -0,0 +1,19 @@
+import type { DeepPartial } from "@reduxjs/toolkit"
+import type { StateSchema } from "App/provider/StoreProvider"
+import { loginSliceActions, loginSliceReducer } from "./loginSlice"
+import type { LoginSchema } from "../types/LoginSchema"
+
+describe("loginSlice.test", () => {
+	test("should ", () => {
+		const state: DeepPartial<StateSchema> = {}
+		expect(loginSliceReducer(state as LoginSchema, loginSliceActions.setUserName("dima"))).toEqual({
+			username: "dima",
+		})
+	})
+	test("should ", () => {
+		const state: DeepPartial<StateSchema> = {}
+		expect(loginSliceReducer(state as LoginSchema, loginSliceActions.setPassword("dima"))).toEqual({
+			password: "dima",
+		})
+	})
+})
