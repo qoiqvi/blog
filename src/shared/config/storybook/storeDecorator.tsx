4840e1af205924863b914badab13011fc452c7a8
qoiqvi <111002830+qoiqvi@users.noreply.github.com>
2023-06-22 18:39:37 +0300

    .gitattributes

--- src/shared/config/storybook/storeDecorator.tsx
+++ src/shared/config/storybook/storeDecorator.tsx
@@ -3,12 +3,15 @@ import { StoreProvider } from "App/provider/StoreProvider"
 import { type StateSchema } from "App/provider/StoreProvider/config/stateSchema"
 import { articleDetailsReducer } from "entities/Article/model/slice/articleDetailsSlice"
 import { profileSliceReducer } from "entities/Profile"
+import { addNewCommentReducer } from "features/AddNewComment/model/slice/addNewCommentSlice"
 import { loginSliceReducer } from "features/AuthByUserName/model/slice/loginSlice"
 
 const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
 	loginForm: loginSliceReducer,
 	profile: profileSliceReducer,
 	articleDetails: articleDetailsReducer,
+	articleDetailsComments: articleDetailsReducer,
+	addNewComment: addNewCommentReducer,
 }
 
 export const StoreDecorator =
